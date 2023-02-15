d902dc2bd7e075f5d3066320800c45cd17704da3
Piper McCorkle <contact@piperswe.me>
2023-02-14 22:41:52 -0600

    Add favicon, minify CSS & HTML

--- scripts/build.js
+++ scripts/build.js
@@ -3,13 +3,25 @@ import * as path from "path";
 import fs from "fs-extra";
 import postcss from "postcss";
 import postcssPreset from "postcss-preset-env";
+import cssnanoPlugin from "cssnano";
+import svgo from "svgo";
+import minifyHtml from "@minify-html/node";
 
 console.log("Copying assets...");
 await fs.remove("dist");
 await fs.mkdir("dist");
 await fs.copy("assets", "dist/assets");
 await fs.copy("index.html", "dist/index.html");
+await fs.copy("favicon.svg", "dist/favicon.svg");
 
+console.log("Converting favicon...");
+await sharp("favicon.svg").toFile("dist/favicon.ico");
+const faviconSvg = await fs.readFile("favicon.svg", { encoding: "utf-8" });
+const optimizedSvg = svgo.optimize(faviconSvg, {
+  path: "favicon.svg",
+  multipass: true,
+}).data;
+await fs.writeFile("dist/favicon.svg", optimizedSvg);
 console.log("Converting images...");
 const images = ["courthouse.jpg"];
 for (const image of images) {
@@ -20,9 +32,8 @@ for (const image of images) {
   await sharp(from).toFile(`dist/assets/images/${path.parse(image).name}.avif`);
 }
 console.log("Running PostCSS...");
-const postcssInstance = postcss([postcssPreset()]);
-// Skip normalize because prefixes are intentioanl
-const stylesheets = ["common.css", "landing.css"];
+const postcssInstance = postcss([postcssPreset(), cssnanoPlugin()]);
+const stylesheets = ["common.css", "landing.css", "normalize.css"];
 for (const stylesheet of stylesheets) {
   console.log(` - ${stylesheet}`);
   const from = `assets/css/${stylesheet}`;
@@ -33,4 +44,14 @@ for (const stylesheet of stylesheets) {
     to,
   });
   await fs.writeFile(`dist/assets/css/${stylesheet}`, processed.css);
+}
+console.log("Minifying HTML...");
+const pages = ["index.html"];
+for (const page of pages) {
+  console.log(` - ${page}`);
+  const from = page;
+  const to = `dist/${from}`;
+  const html = await fs.readFile(from);
+  const minified = minifyHtml.minify(html, {});
+  await fs.writeFile(to, minified);
 }
